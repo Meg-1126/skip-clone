1df6e6df097627716ed635e146e5b28c50e29e71
Megumi Akama <kamayatu91@gmail.com>
2023-02-08 03:38:39 -0800

    Create download app section

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -5,30 +5,37 @@ import { RiGooglePlayFill } from "react-icons/ri";
 export default function Download() {
   return (
     <div className="wrapper__dl">
-      <img
-        src="./images/mobile-en.png"
-        className="img__sp"
-        alt="Two smartphones"
-      />
-      <h2>Download Our App</h2>
-      <p>
-        Our app gives you access to your favourite restaurants. Get your food
-        delivered right to you.
-      </p>
-      <div className="wrapper__dl__btns">
-        <div className="btn__dl__apple">
-          <div>
-            <AiFillApple />
+      <div className="wrapper__dl__img">
+        <img
+          src="./images/mobile-en.png"
+          className="img__sp"
+          alt="Two smartphones"
+        />
+      </div>
+      <div className="wrapper__dl__text">
+        <h2 className="h2__dl">
+          Download <br />
+          Our App
+        </h2>
+        <p>
+          Our app gives you access to your favourite restaurants. Get your food
+          delivered right to you.
+        </p>
+        <div className="wrapper__dl__btns">
+          <div className="btn__dl btn__dl__apple">
+            <div>
+              <AiFillApple />
+            </div>
+            <p>Download on the</p>
+            <p>App Store</p>
           </div>
-          <p>Download on the</p>
-          <p>App Store</p>
-        </div>
-        <div className="btn__dl__google">
-          <div>
-            <RiGooglePlayFill />
+          <div className="btn__dl btn__dl__google">
+            <div>
+              <RiGooglePlayFill />
+            </div>
+            <p>GET IT ON</p>
+            <p>Google Play</p>
           </div>
-          <p>GET IT ON</p>
-          <p>Google Play</p>
         </div>
       </div>
     </div>
